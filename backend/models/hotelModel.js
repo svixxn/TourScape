71a0d68ddf887c00437660198a98a9e7f30b10ea
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-04-24 23:21:05 +0300

    modified a hotel model
    Took 12 minutes

--- backend/models/hotelModel.js
+++ backend/models/hotelModel.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 const hotelSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, "A destination must have a name"],
+        required: [true, "A hotel must have a name"],
         unique: true,
         trim: true,
-        maxLength: [40, "The destination name must have at most 40 characters"],
-        minLength: [10, "The destination name must have at least 10 characters"]
+        maxLength: [40, "The hotel name must have at most 40 characters"],
+        minLength: [10, "The hotel name must have at least 10 characters"]
     },
     description: {
         type: String,
         trim: true,
         required: true,
-        minLength: [30, "The destination description must have at least 30 characters"]
+        minLength: [30, "The hotel description must have at least 30 characters"]
     },
     room_types: [
         {
@@ -21,14 +21,30 @@ const hotelSchema = new mongoose.Schema({
             default: 'Standard'
         }
     ],
-
     location: {
         type: String,
         trim: true,
         required: true
-    }
+    },
+    destinations: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Destination'
+        }
+    ],
+    slug:  String
+});
 
 
+hotelSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'hotel',
+    localField: '_id'
+});
+
+hotelSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 const Hotel = mongoose.model('Hotel', hotelSchema)
