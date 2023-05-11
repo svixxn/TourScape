1f8bc9b5408707e15171f1db9efd1ea5272adb91
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-05-11 22:27:30 +0300

    added header to map v8

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -36,10 +36,10 @@ const sendErrorDev = (err, req, res) => {
     }
 
     console.error('ERROR 💥', err);
-    return res.status(err.statusCode).render('error', {
-        title: 'Something went wrong!',
-        msg: err.message
-    });
+    // return res.status(err.statusCode).render('error', {
+    //     title: 'Something went wrong!',
+    //     msg: err.message
+    // });
 };
 
 const sendErrorProd = (err, req, res) => {
@@ -61,20 +61,20 @@ const sendErrorProd = (err, req, res) => {
     }
 
 
-    if (err.isOperational) {
-        console.log(err);
-        return res.status(err.statusCode).render('error', {
-            title: 'Something went wrong!',
-            msg: err.message
-        });
-    }
+    // if (err.isOperational) {
+    //     console.log(err);
+    //     return res.status(err.statusCode).render('error', {
+    //         title: 'Something went wrong!',
+    //         msg: err.message
+    //     });
+    // }
 
-    console.error('ERROR 💥', err);
+    // console.error('ERROR 💥', err);
 
-    return res.status(err.statusCode).render('error', {
-        title: 'Something went wrong!',
-        msg: 'Please try again later.'
-    });
+    // return res.status(err.statusCode).render('error', {
+    //     title: 'Something went wrong!',
+    //     msg: 'Please try again later.'
+    // });
 };
 
 module.exports = (err, req, res, next) => {
