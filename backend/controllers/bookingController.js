4ad5a055242b84a03bbce23770b7333c253f2743
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-05-26 16:07:07 +0300

    stripe fix v4

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -35,6 +35,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
    const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
+      customer_email: req.user.email,
       customer: req.user._id,
       success_url: successURL,
       client_reference_id: item._id,
