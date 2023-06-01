e0db3f5234c722b013f9e78410a6f90efef0c6d5
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-06-01 16:49:41 +0300

    added available places v20

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -65,12 +65,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const updateAvailablePlaces = catchAsync(async (date, numberOfPeople,client_reference_id) => {
-   const tour = await Tour.findById(client_reference_id).lean()
+   const tour = await Tour.findById(client_reference_id)
    const newDate = new Date(date).toISOString()  
 
+   console.log(newDate, typeof newDate)
    tour.startDates.forEach(date => {
-      console.log(date.date.toString().toLowerCase())
-      console.log(date.date.toString().toLowerCase() === newDate.toString().toLowerCase())
+      const testDate = date.date
+      console.log(testDate, typeof testDate)
    })
 
    console.log(tour.startDates.find(sd => sd.date === newDate))
