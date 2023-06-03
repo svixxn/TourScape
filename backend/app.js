7e82a8b1e4eb87256c36305a42e0d864f5059635
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2023-06-03 13:22:09 +0300

    upload change v3

--- backend/app.js
+++ backend/app.js
@@ -100,8 +100,8 @@ app.post(
     bookingController.checkoutWebhook
 );
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
